8420d2fccedf7ad7675e905af8ac43c65d77f49d
Adeyemi987 <tubosun.adeyemi@decagon.dev>
2022-05-24 23:43:39 +0100

    Setting Up the Environment

---
+++ BankApi/Models/Account.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankApi.Models
+{
+    [Table("Accounts")]
+    public class Account
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AccountName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public decimal CurrentAccountBalance { get; set; }
+        public AccountType AccountType { get; set; } //Enums (Savings or current)
+        public string AccountNumberGenerated { get; set; }
+
+        //store hash and salt of the acct transaction pin
+        public byte[] PinHash { get; set; }
+        public byte[] PinSalt { get; set; }
+
+        public DateTime DateCreated { get; set; }
+        public DateTime DateLastUpdated { get; set; }
+
+        //I want to generate an accountnumber in the below constructor
+        //Generate a randon obj 
+
+        Random rand = new Random();
+        public Account()
+        {
+            AccountNumberGenerated = Convert.ToString((long)rand.NextDouble() * 9_000_000_000L + 1_000_000_000);
+            //AccountName Property = FirstName + LastName
+            AccountName = $"{FirstName} {LastName}";
+
+
+        }
+    }
+
+    public enum AccountType
+    {  
+        Savings,
+        current,
+        Corporate,
+        Government
+    }
+}
---
+++ BankApi/Models/Transaction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankApi.Models
+{
+    [Table("Transactions")]
+    public class Transaction
+    {
+        [Key]
+        public int Id { get; set; }
+        public string TransactionUniqueReference { get; set; } // Generates at every instance of this class
+        public decimal TransactionAmount { get; set; }
+        public TranStatus TransactionStatus { get; set; }
+        public bool IsSuccessful => TransactionStatus.Equals(TranStatus.Success);
+        public string TransactionSourceAccount { get; set; }
+        public string DestinationAccount { get; set; }
+        public string TransactionParticulars { get; set; }
+        public TransType TransactionType { get; set; }
+        public DateTime TransactionDate { get; set; }
+
+
+    }
+
+    public enum TranStatus
+    {
+        Failed,
+        Success,
+        Error
+    }
+
+    public enum TransType
+    {
+        Deposit,
+        Withdrawl,
+        Transfer
+    }
+}
